75374f2f024ba4a939bb387017eb1e14032aa4c3
Szymon <sz.machura@student.uj.edu.pl>
2025-02-18 17:12:25 +0100

    saveSignal func working

--- signalAn.C
+++ signalAn.C
@@ -5,7 +5,9 @@
 /*The first two arguments are for specifying the filename, its format should be "[fileID]_scope_[no].csv".*/
 
 Bool_t saveSignal(std::string fileID = "20250217", std::string no = "21"){
+    Bool_t debug = kFALSE;
     gSystem->Load("mySignal_cxx.so");
+    std::cout.precision(12);
 
     std::string filename = Form("%s_scope_%s.csv", fileID.c_str(), no.c_str());
     std::ifstream data;
@@ -24,33 +26,47 @@ Bool_t saveSignal(std::string fileID = "20250217", std::string no = "21"){
     signTree->Branch(branchName.c_str(), &ms);
 
     Double_t aMax, aThr, t0, t1, Q, TOT;
+    Int_t t0i, t1i;
     Int_t baselineCount = 100;
 
-    Double_t time, voltage;
+    double time, voltage;
     int j = 0;
 
     Int_t nSamples;
     std::string dummy;
     data>>dummy>>dummy>>nSamples;
-    std::cout<<nSamples<<std::endl;
+    std::cout<<"The number of samples in a single signal: "<<nSamples<<std::endl;
 
-    data.ignore(10000, '\n');
-    data.ignore(10000, '\n');
+    std::string line;
 
-    while(!data){
-        TH2D *signHisto = new TH2D(Form("histo%i", j), Form("histo%i", j), nSamples, 0, nSamples, 0, 10);
-        std::vector <Double_t> baseline;
+    getline(data, line);
+    getline(data, line);
+    getline(data, line);
+
+    while(data){
+        TH1D *signHisto = new TH1D(Form("histo%i", j), Form("histo%i", j), nSamples, 0, nSamples);
+        std::vector <Double_t> baseline = {};
+        std::vector <Double_t> times;
 
         for(Int_t i = 0; i<nSamples; i++){
-            data>>time>>voltage;
-            signHisto->SetBinContent(time, voltage);
+            std::getline(data, line, ',');
+            time = line.empty() ? 0 : stod(line);
+            std::getline(data, line, '\n');
+            voltage = line.empty() ? 0 : stod(line);
+
+            signHisto->SetBinContent(i, voltage);
             if(i<baselineCount){
                 baseline.push_back(voltage);
             }
+            times.push_back(time);
         }
+        signHisto->SetBins(nSamples, times.at(0), times.at(times.size()-1));
 
+        /*Calculating the parameters from the signals*/
+        //1. amplitude
         aMax = signHisto->GetMaximum();
 
+        //2. amplitude threshold
         Double_t sum = 0;
         for (Double_t v : baseline) {
             sum += v;
@@ -63,29 +79,43 @@ Bool_t saveSignal(std::string fileID = "20250217", std::string no = "21"){
         }
         variance /= baseline.size();
         Double_t stddev = std::sqrt(variance);
-        aThr = 3*stddev;
+        aThr = 5*stddev;
+        if(debug == true){std::cout<<aThr<<std::endl;}
 
-        for(Int_t i = 0; i<nSamples; i++){
-            t0 = signHisto->GetBinContent(i);
-            if(t0>aThr){
-                for(Int_t k = i; k<nSamples; k++){
-                    t1 = signHisto->GetBinContent(k);
-                    if(t1<aThr){
-                        break;
-                    }
-                }
+        //3. t0&TOT
+        Int_t l = 1;
+        for(Int_t i = 1; i<=nSamples; i++){
+            Double_t vAtI = signHisto->GetBinContent(i);
+            if(vAtI>aThr){
+                if(debug == true){cout<<i<<endl;}
+                t0 = times.at(i-1);
+                break;
+            }
+            l++;
+        }
+        for(Int_t k = l+10; k<=nSamples; k++){
+            Double_t vAtI = signHisto->GetBinContent(k);
+            if(vAtI<aThr){
+                t1 = times.at(k-1);
+                if(debug == true){cout<<k<<endl;}
+                break;
             }
         }
+        std::setprecision(10);
 
+        if(debug == true){cout<<t1<<" "<<t0<<endl;}
         TOT = t1 - t0;
-        Q = signHisto->Integral(t0, t1, 0, aMax);
 
-        std::cout<<"Histo no. "<<j+1<<std::endl;
+        // 4. Charge
+        Int_t binT0 = signHisto->FindBin(t0);
+        Int_t binT1 = signHisto->FindBin(t1);
+
+        Q = signHisto->Integral(binT0, binT1);
 
         ms->set(t0, TOT, aMax, Q);
         signTree->Fill();
-        signHisto->Write();
-        signHisto->Delete();
+        if(debug == true){signHisto->Write();}
+        delete signHisto;
         j++;
     }
     signTree->Write();
